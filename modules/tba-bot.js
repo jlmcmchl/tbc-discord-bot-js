c9f968e74b24b34994cfdbea2616ccfa449d0165
Jordan McMichael <jmcmichael@plex.com>
2017-10-23 14:23:11 -0400

    no more for loops

--- modules/tba-bot.js
+++ modules/tba-bot.js
@@ -11,51 +11,46 @@ class initTBABot extends AbstractModule {
     return [{
       'key': 'message',
       'callback': message => {
+        var date
+        date = new Date()
         message.content.replace(team, (a, b, c, d) => {
-          var lEvent;
-          var lCode;
-          var lTime;
-          var date;
-          date = new Date();
-          lTime = new Date(0, 1, 1);
           this.tba.TeamEvents(b, date.getFullYear(), 'simple', (err, events) => {
             if (err) {
               console.log(err); return;
             }
 
-            for (var i in events) {
-              var event
+            var event = events.reduce((lEvent, cEvent) => {
+              var ld;
               var sd
               var ed
               var t
-              event = events[i];
-              sd = Date.parse(event['start_date']);
-              ed = new Date(event['end_date']);
+              ld = Date.parse(lEvent['end_date']);
+              sd = Date.parse(cEvent['start_date']);
+              ed = new Date(cEvent['end_date']);
               ed = new Date(ed.getFullYear(), ed.getMonth(), ed.getDate() + 1).getTime();
-              t = new Date().getTime();
+              t = date.getTime();
 
-              if ((sd < t && t < ed) || (lTime < ed && ed < t)) {
-                lTime = ed;
-                lEvent = event['name'];
-                lCode = event['event_code'];
+              if ((sd < t && t < ed) || (ld < ed && ed < t)) {
+                return cEvent;
               }
-            }
+              return lEvent;
+            });
 
-            this.tba.TeamEventStatus(b, new Date().getFullYear() + lCode, (err, status) => {
+            this.tba.TeamEventStatus(b, new Date().getFullYear() + event['event_code'], (err, status) => {
               if (err) {
                 console.log(err);
                 console.log(status);
                 return;
               }
               if (status == null) {
-                message.channel.send(`It looks like ${lEvent} hasn't started yet. Check back once matches have begun for a status update!`);
+                message.channel.send(`It looks like ${event['name']} hasn't started yet. Check back once matches have begun for a status update!`);
                 return;
               }
 
               var report = status['overall_status_str'];
               report = report.replace(/<\/?b>/g, '**');
 
-              message.channel.send(`At ${lEvent}, ${report}`);
+              message.channel.send(`At ${event['name']}, ${report}`);
             });
           });
         });
