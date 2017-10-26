5e9818873cd756187c7c15db12959aa5f401fc30
Jordan McMichael <jmcmichael@plex.com>
2017-10-26 18:21:18 -0400

    Mostly complete implementation of GriffBot. TODO, implement endpoint for searching index.

--- modules/AbstractModule.js
+++ modules/AbstractModule.js
@@ -2,12 +2,16 @@ class AbstractModule {
   constructor() {
     this.dClient = null;
     this.pgClient = null;
+    this.esClient = null;
   }
   setDClient(client) {
     this.dClient = client;
   }
   setPGClient(client) {
     this.pgClient = client;
+  }
+  setESClient(client) {
+    this.esClient = client;
   }
   getEvents() {
     return [];
---
+++ modules/GriffBot.js
@@ -0,0 +1,88 @@
+const AbstractModule = require('./AbstractModule');
+
+class GriffBot extends AbstractModule {
+  getEvents() {
+    return [
+      {
+        'key': 'message',
+        'callback': message => {
+          if (!this.isGriff(message.author)) {
+            return;
+          }
+          this.archiveEvent('message', message);
+        }
+      },
+      {
+        'key': 'messageDelete',
+        'callback': message => {
+          if (!this.isGriff(message.author)) {
+            return;
+          }
+          this.archiveEvent('messageDelete', message);
+        }
+      },
+      {
+        'key': 'messageDeleteBulk',
+        'callback': messages => {
+          messages.map(message => {
+            if (!this.isGriff(message.author)) {
+              return;
+            }
+            this.archiveEvent('messagesDeleteBulk', message);
+          });
+        }
+      },
+      {
+        'key': 'messageUpdate',
+        'callback': (oldMessage, newMessage) => {
+          if (!this.isGriff(oldMessage.author)) {
+            return;
+          }
+          this.archiveEvent('messageUpdate', newMessage);
+        }
+      }];
+  }
+
+  isGriff(obj) {
+    return obj.id == process.env.GRIFF_ID;
+  }
+
+  archiveEvent(event, message) {
+    var content = {
+      event: event,
+      message: {
+        guild: {
+          id: message.member.guild.id,
+          name: message.member.guild.name
+        },
+        channel: {
+          id: message.channel.id,
+          name: message.channel.name
+        },
+        author: {
+          id: message.author.id,
+          name: message.author.username
+        },
+        id: message.id,
+        type: message.type,
+        time: message.editedTimestamp || message.createdTimestamp,
+        content: message.content
+      }
+    };
+
+    this.esClient.index({
+      index: 'griffin',
+      type: 'message',
+      body: content
+    }, (err, res) => console.log(err, res));
+  }
+
+  //TODO implement search on es index
+  getEndpoints() {
+    return {
+      '/gSearch': (request, response) => response.send("Hello World!"),
+    };
+  }
+}
+
+module.exports = GriffBot;
