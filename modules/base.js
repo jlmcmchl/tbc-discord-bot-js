9728f02843ae55b4c00e5c263c326fa96badbcb6
Jordan McMichael <jmcmichael@plex.com>
2017-10-26 18:21:18 -0400

    Mostly complete implementation of GriffBot. TODO, implement endpoint for searching index.

--- modules/base.js
+++ modules/base.js
@@ -1,9 +1,6 @@
 const AbstractModule = require('./AbstractModule');
 
 class Base extends AbstractModule {
-  constructor() {
-    super();
-  };
   getEndpoints() {
     return {
       '/': (request, response) => response.send("Hello World!"),
