e13c93e8b5c899a2939ac0ff553850a1df5900ac
Jordan McMichael <jmcmichael@plex.com>
2017-10-31 10:53:29 -0400

    Added tracking for another user.

--- modules/GriffBot.js
+++ modules/GriffBot.js
@@ -44,7 +44,7 @@ class GriffBot extends AbstractModule {
   }
 
   isGriff(obj) {
-    return obj.id == process.env.GRIFF_ID;
+    return obj.id == process.env.GRIFF_ID || obj.id == process.env.JOE_ID;
   }
 
   archiveEvent(event, message) {
