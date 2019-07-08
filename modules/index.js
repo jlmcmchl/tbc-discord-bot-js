095f17f63a781c607b5d686e75095d55757b5221
Jordan McMichael <jlmcmchl@gmail.com>
2019-07-07 21:05:17 -0400

    further cleanup while migrating to elasticsearch

--- modules/index.js
+++ modules/index.js
@@ -1,6 +1,5 @@
 var Base = require('./base');
 var Ds = require('./schedule-draft');
-var Quote = require('./quote');
 var GriffBot = require('./GriffBot');
 var magicBall = require('./magicBall');
 var nick = require("./nicknames");
@@ -10,7 +9,6 @@ class Modules {
     this.modules = [
       new Base(),
       new Ds(),
-      new Quote(),
       new GriffBot(),
       new magicBall(),
       new nick(),
